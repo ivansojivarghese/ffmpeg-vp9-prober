c45f98b45a8fe58181af0a2d9c9f0ddd246ba948
Ivan Soji Varghese <ivansojivarghese@gmail.com>
2025-04-17 23:07:47 +0800

    Update ffprobe.js

--- api/ffprobe.js
+++ api/ffprobe.js
@@ -25,10 +25,19 @@ export default async function handler(req, res) {
 
     if (url.includes('googlevideo.com')) {
       const response = await fetch(url, {
+        /*
         headers: {
           'User-Agent': 'Mozilla/5.0',
           'Referer': 'https://www.youtube.com',
-        },
+        },*/
+        headers: {
+          'User-Agent':
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 ' +
+            '(KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+          'Referer': 'https://www.youtube.com/',
+          'Accept': '*/*',
+          'Accept-Language': 'en-US,en;q=0.9',
+        }
       });
 
       if (!response.ok) {
