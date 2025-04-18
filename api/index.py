770063bef0a2a42117e47e58c53c18fa4538d414
Ivan Soji Varghese <ivansojivarghese@gmail.com>
2025-04-19 01:21:58 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -78,7 +78,7 @@ class handler(BaseHTTPRequestHandler):
                 # Extract video info without downloading
                 info = ydl.extract_info(url, download=False)
 
-            '''
+            
             # Check if 'formats' are available in the extracted data
             formats = [
                 {
@@ -97,10 +97,10 @@ class handler(BaseHTTPRequestHandler):
                 'title': info.get('title'),
                 'formats': formats
             })
-            '''
+            
 
             # Return full info
-            return self._send_json(200, info)
+            # return self._send_json(200, info)
 
         except Exception as e:
             return self._send_json(500, {
